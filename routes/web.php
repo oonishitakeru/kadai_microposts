bb07b7fc93abcb729d53294a4cb309c6c14e4d6b
takeru.oonishi <takeru.oonishi@techacademy.jp>
2023-05-01 04:24:16 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsersController; // 追記
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\MicropostsController;// 追記
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +16,9 @@ use App\Http\Controllers\UsersController; // 追記
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', [MicropostsController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 /*Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,5 +29,11 @@ Route::get('/dashboard', function () {
 Route::group(['middleware' => ['auth']], function () {                                    // 追記
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記
 });  
+
+Route::group(['middleware' => ['auth']], function () {
+    // 中略
+    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
+    Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
+});
 
 require __DIR__.'/auth.php';
